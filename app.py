c334d49c9192954c3af2cc337e78059b8b071d8f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-06 02:27:10 -0500

    update

--- app.py
+++ app.py
@@ -159,15 +159,15 @@ with gr.Blocks() as demo:
                     gen_len = gr.Number(label="Generate N seconds", value=10)
                     speakers = gr.Radio(label="Number of Speakers", choices=[1,2], value=1)
                 button = gr.Button("Generate")
+                with gr.Accordion("Advanced", open=False):
+                    token_temp = gr.Number(label="token temperature", value=0.8)
+                    categorical_temp = gr.Number(label="categorical temperature", value=0.4)
+                    gaussian_temp = gr.Number(label="gaussian temperature", value=0.1)
                 button.click(
                     fn=run,
                     inputs=[audio, prompt_len_seconds, gen_len, speakers, token_temp, categorical_temp, gaussian_temp],
                     outputs=[generated]
                 )
-                with gr.Accordion("Advanced", open=False):
-                    token_temp = gr.Number(label="token temperature", value=0.8)
-                    categorical_temp = gr.Number(label="categorical temperature", value=0.4)
-                    gaussian_temp = gr.Number(label="gaussian temperature", value=0.1)
         with gr.Column():
             generated = gr.Audio(label="Generated", type="filepath", interactive=False)
     
